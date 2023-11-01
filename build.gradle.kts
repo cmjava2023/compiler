7a7b32457c958c3a6e7d0fbb4e4cad3d6debc442
ironeer <maximiliankias@gmx.de>
2023-11-01 18:31:11 +0100

    run javac --release 8 and javap using gradle not github

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,48 @@ dependencies {
 }
 
 tasks.test {
+    dependsOn("prepareTestFilesWithJdk8")
     useJUnitPlatform()
+}
+
+tasks.register("prepareTestFilesWithJdk8") {
+    doFirst {
+        val outputRoot = File("build/test-classfiles")
+        val testJavaFilesRoot = File("src/test/resources/java")
+
+        outputRoot.deleteRecursively()
+        outputRoot.mkdirs()
+
+        println("Compile Java test files using jdk version 8. Files from $testJavaFilesRoot compiled to $outputRoot.")
+        testJavaFilesRoot.walk().forEach {
+            if (it.extension == "java") {
+                val outputFolder = File(outputRoot, it.parentFile.relativeTo(testJavaFilesRoot).path)
+                outputFolder.mkdirs()
+                val commandParts = listOf("javac", it.path, "-d", outputFolder.path, "--release",  "8")
+                println("  " + commandParts.joinToString(" "))
+                ProcessBuilder(commandParts)
+                        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                        .redirectError(ProcessBuilder.Redirect.INHERIT)
+                        .start()
+                        .waitFor()
+            }
+        }
+
+        println()
+        println("Save javap output")
+
+        outputRoot.walk().forEach {
+            if (it.extension == "class") {
+                val commandParts = listOf("javap", "-c", "-p", it.path)
+                println("  " + commandParts.joinToString(" "))
+                val outputFile = File(it.parentFile, it.nameWithoutExtension + ".javap.txt")
+                println("  Output:$outputFile")
+                ProcessBuilder(commandParts)
+                        .redirectOutput(outputFile)
+                        .redirectError(ProcessBuilder.Redirect.INHERIT)
+                        .start()
+                        .waitFor()
+            }
+        }
+    }
 }
--- src/test/resources/java/helloWorld/Main.java
+++ src/test/resources/java/helloWorld/Main.java
@@ -1,3 +1,5 @@
+package org.cmjava2023;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
