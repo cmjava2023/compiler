dc64897ec3d2271fb22c14bab498f8baa4ff8064
ironeer <maximiliankias@gmx.de>
2024-01-11 08:43:00 +0100

    change continue test file to use while instead of for

--- src/test/resources/java-test-files/cmjava2023/controlFlow/continue_test/Main.java
+++ src/test/resources/java-test-files/cmjava2023/controlFlow/continue_test/Main.java
@@ -2,7 +2,9 @@ package cmjava2023.controlFlow.continue_test;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        int i = 1;
+        while (i <= 5) {
+            i++;
             if (i == 3) {
                 continue;
             }
